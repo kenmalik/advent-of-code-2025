44d6708bdbae032b0696257e95f61cd1838b33b5
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-12-05 00:53:48 -0800

    Add day 2 part 2 solution

--- day_02/solution.py
+++ day_02/solution.py
@@ -5,13 +5,19 @@ input_file = f"{module_dir}/input.txt"
 
 
 def part_1():
+    check_ranges(has_double_repeat)
+
+
+def part_2():
+    check_ranges(has_repeats)
+
+
+def check_ranges(check):
     total = 0
     for r in read_ranges():
         start, end = (int(val) for val in r.split("-"))
-        print(start, end)
         for i in range(start, end + 1):
-            if has_repeat(str(i)):
-                print(f"  {i}")
+            if check(str(i)):
                 total += i
 
     print("Total:", total)
@@ -32,12 +38,33 @@ def read_ranges():
                 r += c
 
 
-def has_repeat(s: str):
+def has_double_repeat(s: str):
     if len(s) % 2 != 0:
         return False
     mid = len(s) // 2
     return s[0:mid] == s[mid:]
 
 
+def has_repeats(s: str):
+    for divisor in range(len(s), 1, -1):
+        if len(s) % divisor != 0:
+            continue
+        pattern_length = len(s) // divisor
+        if has_repeating_pattern(s, pattern_length):
+            return True
+
+    return False
+
+
+def has_repeating_pattern(s: str, pattern_length: int):
+    pattern = s[0:pattern_length]
+    for start in range(pattern_length, len(s), pattern_length):
+        segment = s[start : start + pattern_length]
+        if segment != pattern:
+            return False
+    return True
+
+
 if __name__ == "__main__":
     part_1()
+    part_2()
