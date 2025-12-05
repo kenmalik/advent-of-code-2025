0e4648aebf3fb413d035e34453b86491d6d1edd1
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-12-04 23:44:05 -0800

    Add day 1 part 2 solution

--- day_01/solution.py
+++ day_01/solution.py
@@ -1,11 +1,17 @@
+from pathlib import Path
+
+module_dir = Path(__file__).parent
+input_file = f"{module_dir}/input.txt"
+
+
 domain_l, domain_r = 0, 100
 
 
-def day_01():
+def part_1():
     position = 50
     zero_count = 0
 
-    with open("input.txt", "r") as f:
+    with open(input_file, "r") as f:
         for line in f:
             direction = line[0]
             amount = int(line[1:-1])
@@ -32,5 +38,43 @@ def rotate(start: int, amount: int, rotate_left: bool = False):
     return position
 
 
+def part_2():
+    position = 50
+    zero_passes = 0
+
+    with open(input_file, "r") as f:
+        for line in f:
+            direction = line[0]
+            amount = int(line[1:-1])
+
+            position, passes = rotate_with_passes(
+                position, amount, rotate_left=direction == "L"
+            )
+            zero_passes += passes
+
+    print("Password:", zero_passes)
+
+
+def rotate_with_passes(start: int, amount: int, rotate_left: bool = False):
+    next = start
+
+    zero_passes = amount // domain_r
+    delta = amount % domain_r
+
+    if rotate_left:
+        delta = -delta
+    next += delta
+
+    if not domain_l <= next < domain_r:
+        if start != 0:
+            zero_passes += 1
+        next %= domain_r
+    elif start != 0 and next == 0:
+        zero_passes += 1
+
+    return next, zero_passes
+
+
 if __name__ == "__main__":
-    day_01()
+    part_1()
+    part_2()
--- day_01/test_solution.py
+++ day_01/test_solution.py
@@ -1,4 +1,5 @@
-from day_01.solution import rotate
+import pytest
+from day_01.solution import rotate, rotate_with_passes
 
 
 def test_rotate():
@@ -13,3 +14,34 @@ def test_rotate():
 
     assert rotate(5, 200) == 5
     assert rotate(5, 200, rotate_left=True) == 5
+
+
+@pytest.mark.parametrize(
+    "start,amount,rotate_left,next_expected,passes_expected",
+    [
+        (5, 5, True, 0, 1),
+        (5, 95, False, 0, 1),
+        (5, 1, True, 4, 0),
+        (5, 1, False, 6, 0),
+        (5, 10, True, 95, 1),
+        (5, 100, False, 5, 1),
+        (5, 200, True, 5, 2),
+        (5, 200, False, 5, 2),
+        (5, 205, True, 0, 3),
+        (5, 295, False, 0, 3),
+        (0, 1, True, 99, 0),
+        (0, 1, False, 1, 0),
+        (0, 100, True, 0, 1),
+        (0, 100, False, 0, 1),
+        (0, 101, True, 99, 1),
+        (0, 101, False, 1, 1),
+        (0, 200, True, 0, 2),
+        (0, 200, False, 0, 2),
+        (0, 201, True, 99, 2),
+        (0, 201, False, 1, 2),
+    ],
+)
+def test_rotate_with_passes(start, amount, rotate_left, next_expected, passes_expected):
+    next, passes = rotate_with_passes(start, amount, rotate_left)
+    assert next == next_expected
+    assert passes == passes_expected
