14ec30b94ef6d7b6d0fbe87b47cd6ffaa50266e8
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-12-07 12:37:09 -0800

    Add day 7 part 2 solution

--- day_07/solution.py
+++ day_07/solution.py
@@ -1,7 +1,13 @@
+from collections import defaultdict
+from functools import reduce
+from operator import add
 from pathlib import Path
 
 module_dir = Path(__file__).parent
 input_file = module_dir / "input.txt"
+
+source = "S"
+splitter = "^"
 
 
 def line_reader():
@@ -14,10 +20,9 @@ def part_1():
     reader = line_reader()
     first_line = next(reader)
 
-    splitter = "^"
     min, max = 0, len(first_line) - 1
 
-    beam_indices = {first_line.find("S")}
+    beam_indices = {first_line.find(source)}
 
     split_count = 0
     for line in reader:
@@ -39,5 +44,37 @@ def part_1():
     print(split_count)
 
 
+def part_2():
+    reader = line_reader()
+    first_line = next(reader)
+
+    min, max = 0, len(first_line) - 1
+
+    beam_indices = defaultdict(int)
+    beam_indices[first_line.find(source)] = 1
+
+    for line in reader:
+        to_add = defaultdict(int)
+        to_delete = set()
+
+        for i, source_timelines in beam_indices.items():
+            if line[i] == splitter:
+                if i + 1 <= max:
+                    to_add[i + 1] += source_timelines
+                if i - 1 >= min:
+                    to_add[i - 1] += source_timelines
+                to_delete.add(i)
+
+        for i, source_timelines in to_add.items():
+            beam_indices[i] += source_timelines
+
+        for i in to_delete:
+            del beam_indices[i]
+
+    total_timelines = reduce(add, beam_indices.values())
+    print(total_timelines)
+
+
 if __name__ == "__main__":
     part_1()
+    part_2()
